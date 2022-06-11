d23f4ba9a6e1c8b0d193ccec6edd95eec83883a6
YtShen416 <91193061+YtShen416@users.noreply.github.com>
2022-06-12 00:04:07 +0800

    Create vue.config.js

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,4 +1,16 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+    transpileDependencies: true
 })
+
+const webpack = require('webpack')
+
+module.exports = {
+
+    chainWebpack: config => {
+        config.plugin('provide').use(webpack.ProvidePlugin, [{
+            'window.Quill': 'quill'
+        }])
+
+    }
+}
