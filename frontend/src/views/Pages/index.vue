33c5f00b875d3b832d69ff735571c7b797768a58
YtShen416 <91193061+YtShen416@users.noreply.github.com>
2022-06-12 10:53:25 +0800

    Update index.vue

--- frontend/src/views/Pages/index.vue
+++ frontend/src/views/Pages/index.vue
@@ -11,7 +11,7 @@
       @focus="onEditorFocus($event)"
       @textChange="onEditorChange($event)"
       @ready="onEditorReady($event)"
-      style="min-height:500px"
+      style="min-height: 500px"
       :options="editorOption"
     />
     <div id="count">当前字数：{{ count }}</div>
@@ -22,28 +22,29 @@
 </template>
 
 <script>
+import {instance} from "@/axios/axios";
 import Header from "../../components/header.vue";
-import { saveAs } from 'file-saver'
-import { pdfExporter } from 'quill-to-pdf'
+import { saveAs } from "file-saver";
+import { pdfExporter } from "quill-to-pdf";
 
-import { reactive, ref, toRefs, toRaw } from "vue";
+import { reactive, ref, toRefs, toRaw, readOnly } from "vue";
 import { useWebSocket } from "../../hooks";
 
 const toolbarOptions = [
-  ['bold', 'italic', 'underline', 'strike'],        // 加粗，斜体，下划线，删除线
-  ['blockquote', 'code-block'],                     //引用，代码块
-  [{ 'header': 1 }, { 'header': 2 }],               // 几级标题
-  [{ 'list': 'ordered' }, { 'list': 'bullet' }],    // 有序列表，无序列表
-  [{ 'script': 'sub' }, { 'script': 'super' }],     // 下角标，上角标
-  [{ 'indent': '-1' }, { 'indent': '+1' }],         // 缩进
-  [{ 'direction': 'rtl' }],                         // 文字输入方向
-  [{ 'size': ['small', false, 'large', 'huge'] }],  // 字体大小
-  [{ 'color': [] }],          // 颜色选择
-  [{ 'font': [] }], // 字体
-  [{ 'align': [] }],    // 居中
-  ['clean'],            // 清除样式,
-  ['link', 'image'],  // 上传图片、上传视频
-]
+  ["bold", "italic", "underline", "strike"], // 加粗，斜体，下划线，删除线
+  ["blockquote", "code-block"], //引用，代码块
+  [{ header: 1 }, { header: 2 }], // 几级标题
+  [{ list: "ordered" }, { list: "bullet" }], // 有序列表，无序列表
+  [{ script: "sub" }, { script: "super" }], // 下角标，上角标
+  [{ indent: "-1" }, { indent: "+1" }], // 缩进
+  [{ direction: "rtl" }], // 文字输入方向
+  [{ size: ["small", false, "large", "huge"] }], // 字体大小
+  [{ color: [] }], // 颜色选择
+  [{ font: [] }], // 字体
+  [{ align: [] }], // 居中
+  ["clean"], // 清除样式,
+  ["link", "image"], // 上传图片、上传视频
+];
 
 export default {
   props: {
@@ -51,7 +52,10 @@ export default {
   },
   setup(props) {
     var docId = window.location.href.split("?")[1].split("=")[1].split("&")[0];
-    var docName = window.location.href.split("?")[1].split("&")[1].split("=")[1];
+    var docName = window.location.href
+      .split("?")[1]
+      .split("&")[1]
+      .split("=")[1];
     var uid = window.location.href.split("?")[2].split("=")[1];
 
     let content = ref("");
@@ -61,17 +65,18 @@ export default {
     const state = reactive({
       text: "",
       count: 0,
-      delta:0,
+      delta: 0,
+    
     });
 
-    const ws = useWebSocket(docId,handleMessage);
+    const ws = useWebSocket(docId, handleMessage);
 
     function handleMessage(e) {
       let quill = toRaw(myQuillEditor.value).getQuill();
-      let deltas=e.data.split('\n');
-      deltas.forEach(element => {
+      let deltas = e.data.split("\n");
+      deltas.forEach((element) => {
         console.log(element);
-        let obj=JSON.parse(element);
+        let obj = JSON.parse(element);
         state.delta++;
         quill.updateContents(obj._delta.ops);
       });
@@ -93,6 +98,27 @@ export default {
       console.log("Editor ready.", e);
 
       let quill = toRaw(myQuillEditor.value).getQuill();
+
+      instance
+        .get('/getPermission', {
+          params: {
+            documentId: docId,
+            userId: uid,
+          },
+          
+        })
+        .then((res) => {
+          console.log('res',res.data);
+          //permission = res.data; 
+          if(res.data == 2) {
+            quill.enable(false);
+          }
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+
+
       quill.on("text-change", (delta) => {
         console.log("change", delta);
         const txt = quill.getText();
@@ -100,24 +126,25 @@ export default {
         state.count = char.length;
 
         //将docId和delta传递给后端
-        if(state.delta==0){
-            ws.send(
+        if (state.delta == 0) {
+          ws.send(
             JSON.stringify({
               _delta: delta,
             })
           );
+        } else {
+          state.delta--;
         }
-        else{state.delta--;}
       });
     };
 
-     async function toPDF() {
+    async function toPDF() {
       let quill = toRaw(myQuillEditor.value).getQuill();
-      const delta = quill.getContents();   //gets the Quill delta
+      const delta = quill.getContents(); //gets the Quill delta
       const pdfAsBlob = await pdfExporter.generatePdf(delta); //converts to PDF
       //saveAs(pdfAsBlob, 'pdf-export.pdf'); //downloads from the browser
       saveAs(pdfAsBlob, docName);
-    } 
+    }
 
     return {
       ...toRefs(state),
@@ -130,31 +157,26 @@ export default {
       toPDF,
     };
   },
-  methods: {
 
-  },
+  methods: {},
 
   components: {
     Header,
-    
   },
 
-  
   data() {
     return {
       content: "",
+      //disabled:false,
       editorOption: {
-        //ImageResize:{},
         theme: "snow",
         modules: {
           toolbar: toolbarOptions,
-     /*      ImageResize:{}, */
         },
+        placeholder: "Start here....",
       },
     };
   },
-
- 
 };
 </script>
 
