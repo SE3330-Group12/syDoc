fc35c10ad3399d8aa9543469d8819d814dce4c16
YtShen416 <91193061+YtShen416@users.noreply.github.com>
2022-06-18 00:41:29 +0800

    add title

--- frontend/src/views/Pages/index.vue
+++ frontend/src/views/Pages/index.vue
@@ -2,7 +2,7 @@
   <div>
     <Header></Header>
     <div id = 'model'>
-    <p>当前模式： {{model}}</p></div>
+    <p>{{docName}} （{{model}}）</p></div>
     <QuillEditor
       id="editorId"
       ref="myQuillEditor"
@@ -93,6 +93,7 @@ export default {
       count: 0,
       delta: 0,
       model:"",
+      docName:"",
     });
     var username="nzy";
     var docType="doc";
@@ -125,6 +126,7 @@ export default {
       console.log("Editor ready.", e);
 
       let quill = toRaw(myQuillEditor.value).getQuill();
+      state.docName = docName;
 
       instance
         .get('/getPermission', {
